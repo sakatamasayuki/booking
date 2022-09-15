9514ece644bfd8524a2527935d9e082dc8abb3d6
sakatamasayuki <ikatako.atom@gmail.com>
2022-09-15 12:02:46 +0900

    add:views,routes
    viewsのページを空作成。ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  # get 'reservations/index'
+  # get 'rooms/index'
+  # get 'users/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "top#index"
+
+  resources :reservations
+  resources :rooms
+  resources :users
+
 end
